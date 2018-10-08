ea26b50fec9c711f0b964772801b98560c9399f6
birendra <33861012+bire0011@users.noreply.github.com>
2018-10-08 12:33:16 +0200

    Add project files.

---
+++ DLLWeightConversion/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DLLWeightConversion
+{
+    public class Class1
+    {
+        public double ConvertToGrams(double onc)
+        {
+            double gm = onc * 28.34952;
+            return gm;
+
+        }
+        
+        public double ConvertToOunces(double gm)
+        {
+            double onc = gm * 0.03527396195;
+            return onc;
+        }
+    }
+}
---
+++ DLLWeightConversionUnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+using DLLWeightConversion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DLLWeightConversionUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Class1 c1= new Class1();
+        
+        [TestMethod]
+        public void ConvertToGramTestMethod()
+        {
+            double ut = c1.ConvertToGrams(2);
+            Assert.AreEqual(56.69904, ut, 0.001);
+        }
+        [TestMethod]
+        public void ConvertToOuncesTestMethod()
+        {
+            double ut = c1.ConvertToOunces(1);
+            Assert.AreEqual(0.035, ut, 0.001);
+        }
+    }
+}
